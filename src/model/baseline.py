064a2c7502a9d8ef630665e68bb748b3bd6b5cd4
Yuriy Antentyk <antentyk@ucu.edu.ua>
2018-12-01 13:19:48 +0200

    Add baseline model for evaluation

---
+++ src/model/baseline.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath("../"))
+
+
+from torch.nn import Module, Linear
+from torch.nn.functional import sigmoid
+from torch.optim import Adam
+from torch.nn.modules.loss import L1Loss
+import torch.nn.modules.loss as loss
+
+
+from config import config
+
+
+class Baseline(Module):
+    def __init__(self):
+        super(Baseline, self).__init__()
+
+        self.layer = Linear(config["n_frequency_bins"], config["notes_number"])
+        self.optimizer = Adam(self.parameters(), lr=1e-3)
+        self.criterion = L1Loss()
+
+    def forward(self, x):
+        x = self.linear(x)
+        x = sigmoid(x)
+        return x
