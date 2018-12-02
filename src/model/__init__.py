cfef8d008015afa08b8c0d5b46a1f664f26f1a40
Yuriy Antentyk <antentyk@ucu.edu.ua>
2018-12-02 13:53:15 +0200

    Change round_probabilities to work with torch.tensor, implement get_metrics

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,3 +1,3 @@
 from .baseline import Baseline
 from .train_model_batch import train_model_batch
-from .utils import round_probabilities
+from .utils import round_probabilities, get_metrics
--- src/model/utils.py
+++ src/model/utils.py
@@ -5,5 +5,22 @@ import numpy as np
 
 from config import config
 
-def round_probabilities(output):
-    return np.vectorize(lambda x: 0 if x < config["round_bound"] else 1)(output)
+
+def round_probabilities(prediction):
+    prediction[prediction < config["round_bound"]] = 0
+    prediction[prediction != 0] = 1
+    prediction = prediction.int()
+
+def get_metrics(prediction, target):
+    true_positive = (prediction * target).sum(dim=1).float()
+    false_positive = (prediction * (target ^ 1)).sum(dim=1).float()
+    false_negative = ((prediction ^ 1) * target).sum(dim=1).float()
+
+    res = {}
+
+    res["precision"] = (true_positive / (true_positive + false_positive)).sum().item()
+    res["recal"] = (true_positive / (true_positive + false_negative)).sum().item()
+    res["accuracy"] = (true_positive / (true_positive + false_positive + false_negative)).sum().item()
+    res["f-measure"] = (2 * res["precision"] * res["recal"]) / (res["precision"] + res["recal"])
+
+    return res
